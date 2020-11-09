33cf9cbe064d5b46e76c4e1d7146f76a9ccda6d4
Dennis Peters <denjpeters@gmail.com>
2020-11-08 19:51:47 -0500

    MySQL commands

--- consoles/consoles.ts
+++ consoles/consoles.ts
@@ -1,3 +1,32 @@
+import mysql from 'mysql'
+import {MySQL} from '../src/Database/MySQL/MySQL'
+
 require('source-map-support').install()
 
-console.log('Console Log 2')
+process.env.MYSQLDATABASE = 'qvsheets-app-local'
+
+const processConsole = async () => {
+	let connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'admin',
+		password: '123',
+		database: process.env.MYSQLDATABASE,
+		port: 33061
+	})
+	
+	connection.connect()
+
+// connection.query('SELECT * FROM account', (error , results, fields) => {
+// 	if (error) throw error
+// 	console.log('The solution is: ', [...results])
+// })
+	
+	console.log(await MySQL.Tables(connection))
+	
+	connection.end()
+}
+
+processConsole()
+	.then(() => {
+		console.log('Done')
+	})
