de465ad6e8a10f0e5bc4d4274a3abc7f57638d6b
Dennis Peters <denjpeters@gmail.com>
2020-12-01 19:12:40 -0500

    Updated MyToPG

--- dist/Database/Conversions/MyToPG.d.ts
+++ dist/Database/Conversions/MyToPG.d.ts
@@ -1,13 +1,17 @@
 import { MyTable } from '../MySQL/MyTable';
-import { PGTable } from '../PGSQL/PGTable';
 import { MyColumn } from '../MySQL/MyColumn';
 import { PGColumn } from '../PGSQL/PGColumn';
 import { MyForeignKey } from '../MySQL/MyForeignKey';
 import { PGForeignKey } from '../PGSQL/PGForeignKey';
 import { MyIndex } from '../MySQL/MyIndex';
 import { PGIndex } from '../PGSQL/PGIndex';
+import { PGTable } from '../PGSQL/PGTable';
+export declare class PGTableMy extends PGTable {
+    myTable?: MyTable;
+    constructor(instanceData?: PGTable, myTable?: MyTable);
+}
 export declare namespace MyToPG {
-    const GetPGTable: (myTable: MyTable) => PGTable;
+    const GetPGTable: (myTable: MyTable) => PGTableMy;
     const GetPGColumn: (myColumn: MyColumn) => PGColumn;
     const GetPGForeignKey: (myForeignKey: MyForeignKey) => PGForeignKey;
     const GetPGIndex: (myIndex: MyIndex) => PGIndex;
