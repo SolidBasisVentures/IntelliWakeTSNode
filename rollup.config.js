76c961aaa61032031892eab0567afbc59d68126c
Dennis Peters <denjpeters@gmail.com>
2022-03-02 17:51:05 -0500

    Include SFTP

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,30 @@
 import pkg from './package.json'
 import typescript from 'rollup-plugin-typescript2'
 import legacy from 'rollup-plugin-legacy'
+import copy from 'rollup-plugin-copy'
 
 export default {
-    input: [
-        'src/main.ts'
-    ],
-    output: [
-        {
-            file: pkg.main,
-            format: 'cjs'
-	        // file: 'dist/main.js',
-	        // format: 'esm'
-        }
-    ],
-    plugins: [typescript({ objectHashIgnoreUnknownHack: false }),
-	    legacy({
-		    "SFTP/index.js": "SFTP"
-	    })],
-    external: ['path', 'fs', 'moment-timezone', 'readline', '@solidbasisventures/intelliwaketsfoundation']
+	input: [
+		'src/main.ts'
+	],
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs'
+			// file: 'dist/main.js',
+			// format: 'esm'
+		}
+	],
+	plugins: [
+		typescript({objectHashIgnoreUnknownHack: false}),
+		legacy({
+			'SFTP/index.js': 'SFTP'
+		}),
+		copy({
+			targets: [
+				{src: 'src/SFTP/**/*', dest: 'dist/SFTP'}
+			]
+		})
+	],
+	external: ['path', 'fs', 'moment-timezone', 'readline', '@solidbasisventures/intelliwaketsfoundation']
 }
