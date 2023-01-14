3aef98fef9eff103f15d3dc3d3217ea7f2d0176a
Dennis Peters <denjpeters@gmail.com>
2023-01-14 11:34:27 -0500

    Update IW

--- src/Database/PGSQL/PGTable.ts
+++ src/Database/PGSQL/PGTable.ts
@@ -11,8 +11,10 @@ import {
 	YYYY_MM_DD_HH_mm_ss
 } from '@solidbasisventures/intelliwaketsfoundation'
 import {PGEnum} from './PGEnum'
-import type {TObjectConstraint,
-	TObjectFieldConstraint} from '@solidbasisventures/intelliwaketsfoundation'
+import type {
+	TObjectConstraint,
+	TObjectFieldConstraint
+} from '@solidbasisventures/intelliwaketsfoundation'
 
 const TS_EOL = '\n' // was \r\n
 
