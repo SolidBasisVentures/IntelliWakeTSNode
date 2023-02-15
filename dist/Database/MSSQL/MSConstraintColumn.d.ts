5e288552525c66c0a8aa106daf6abce8efa2713d
Dennis Peters <denjpeters@gmail.com>
2023-02-15 15:44:28 -0500

    Update IW

--- dist/Database/MSSQL/MSConstraintColumn.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class MSConstraintColumn {
-    TABLE_NAME: string;
-    COLUMN_NAME: string;
-    CONSTRAINT_NAME: string;
-    isPrimaryKey: boolean;
-    constructor(instanceData?: MSConstraintColumn);
-    private deserialize;
-}
