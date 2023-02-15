61f946ba65aa043e70c35c21c4939501c7401fb9
Dennis Peters <denjpeters@gmail.com>
2023-02-15 15:30:44 -0500

    remove unused fills

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import {resolve} from 'path'
 import {defineConfig} from 'vitest/config'
 import dts from 'vite-plugin-dts'
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 export default defineConfig({
 	test: {},
-	resolve: {
-		alias: {
-			child_process: ""
-		}
-	},
 	build: {
-		target: "es2015",
+		target: 'es2015',
 		lib: {
 			entry: resolve(__dirname, 'src/main.ts'),
 			name: 'main',
@@ -19,7 +13,6 @@ export default defineConfig({
 		}
 	},
 	plugins: [
-		dts(),
-		rollupNodePolyFill()
+		dts()
 	]
 })
