dbe27ce8c4e4821dbd5ec77dcfba48aef53364fb
Dennis Peters <denjpeters@gmail.com>
2023-02-15 15:17:58 -0500

    Update git ignore

--- dist/Database/PGSQL/PGWhereSearchClause.d.ts
+++
@@ -1,2 +0,0 @@
-import { PGParams } from './PGParams';
-export declare const PGWhereSearchClause: (search: string | null | undefined, params: PGParams, fields: string[], startWithAnd?: boolean) => string;
