a86925f9e42804ac225e1d3d29017c9209725a51
Dennis Peters <denjpeters@gmail.com>
2023-02-15 14:45:53 -0500

    Removed dist

--- dist/SortSearch.d.ts
+++
@@ -1,5 +0,0 @@
-import { IPaginatorRequest, IPaginatorResponse } from '@solidbasisventures/intelliwaketsfoundation';
-export declare const PaginatorResponseFromRequestCount: <SORT = Record<string, any>, FILTER = Record<string, any>>(paginatorRequest: IPaginatorRequest<SORT, FILTER>, rowCount: number) => IPaginatorResponse<SORT>;
-export declare const PaginatorInitializeResponseFromRequest: <SORT = Record<string, any>, FILTER = Record<string, any>, RES = Record<string, any>>(paginatorRequest: IPaginatorRequest<SORT, FILTER>) => IPaginatorResponse<RES>;
-export declare const PaginatorApplyRowCount: <T = Record<string, any>>(paginatorResponse: IPaginatorResponse<T>, rowCount: number) => void;
-export declare const PaginatorReturnRowCount: <T = Record<string, any>>(paginatorResponse: IPaginatorResponse<T>, rowCount: number) => IPaginatorResponse<T>;
