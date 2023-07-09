360dd03dc1dd410b6235442ab9fa17d50e1ff0ff
Dennis Peters <denjpeters@gmail.com>
2023-07-09 18:43:15 -0400

    Testing conditions for promise resolution

--- consoles/consoles.ts
+++ consoles/consoles.ts
@@ -1,122 +1,23 @@
-import {PGTable} from '../src/Database/PGSQL/PGTable'
-import {PGColumn} from '../src/Database/PGSQL/PGColumn'
+import {testValue} from './TestData'
 
-
-const processScript = async () => {
-	const pgTable = new PGTable()
-
-	pgTable.importWithTypes = true
-
-	pgTable.name = 'test_table'
-
-	pgTable.addColumn({
-		column_name: 'id',
-		udt_name: PGColumn.TYPE_INTEGER,
-		is_nullable: 'NO'
-	})
-	pgTable.addColumn({
-		column_name: 'added_id',
-		udt_name: PGColumn.TYPE_INTEGER,
-		is_nullable: 'YES'
-	})
-	pgTable.addColumn({
-		column_name: 'name',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		is_nullable: 'YES',
-		character_maximum_length: 20
-	})
-	pgTable.addColumn({
-		column_name: 'description',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		is_nullable: 'YES',
-		character_maximum_length: 30
-	})
-	pgTable.addColumn({
-		column_name: 'address',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		is_nullable: 'YES',
-		character_maximum_length: 15
-	})
-	pgTable.addColumn({
-		column_name: 'phone',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		is_nullable: 'YES',
-		character_maximum_length: 9
-	})
-	pgTable.addColumn({
-		column_name: 'added_date',
-		is_nullable: 'NO',
-		udt_name: PGColumn.TYPE_DATE
-	})
-	pgTable.addColumn({
-		column_name: 'enum_test_null',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		character_maximum_length: 64,
-		is_nullable: 'YES',
-		column_comment: '{enum: ETest}'
-	})
-	pgTable.addColumn({
-		column_name: 'enum_test_null_default',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		character_maximum_length: 64,
-		is_nullable: 'YES',
-		column_default: 'FirstValue',
-		column_comment: '{enum: ETest}'
-	})
-	pgTable.addColumn({
-		column_name: 'enum_test2_null_default',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		character_maximum_length: 64,
-		is_nullable: 'YES',
-		column_comment: '{enum: ETest2}'
-	})
-	pgTable.addColumn({
-		column_name: 'enum_test_null_default_comment',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		character_maximum_length: 64,
-		is_nullable: 'YES',
-		column_comment: '{enum: ETest.FirstValue}'
-	})
-	pgTable.addColumn({
-		column_name: 'enum_test_default',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		character_maximum_length: 64,
-		is_nullable: 'NO',
-		column_default: 'FirstValue',
-		column_comment: '{enum: ETest}'
-	})
-	pgTable.addColumn({
-		column_name: 'enum_test_default_comment',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		character_maximum_length: 64,
-		is_nullable: 'NO',
-		column_comment: '{enum: ETest.FirstValue}'
-	})
-	pgTable.addColumn({
-		column_name: 'interface_test_null',
-		udt_name: PGColumn.TYPE_JSON,
-		is_nullable: 'YES',
-		column_comment: '{interface: ITest}'
-	})
-	pgTable.addColumn({
-		column_name: 'enum_test3_array_null',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		array_dimensions: [1],
-		column_default: '\'{}\'::character varying[]',
-		character_maximum_length: 64,
-		is_nullable: 'NO',
-		column_comment: '{enum: ETest3}'
-	})
-	pgTable.addColumn({
-		column_name: 'enum_test4_null',
-		udt_name: PGColumn.TYPE_VARCHAR,
-		column_default: '',
-		character_maximum_length: 64,
-		column_comment: '{enum: ETest4.Test}'
-	})
-
-	console.log(pgTable.tsText())
-	// console.log(pgTable.tsTextTable())
+function testValueFunc() {
+	return testValue
 }
 
-processScript().then(() => console.log('Done!'))
+async function testValueFuncAS(): Promise<number> {
+	return new Promise<number>(resolve => {
+		resolve(testValue)
+	})
+}
+
+const processScript = async () => {
+	const tvf = testValueFunc()
+	const tvfas = testValueFuncAS()
+
+	console.log('tvf', await Promise.resolve(tvf))
+	console.log('tvfas', await Promise.resolve(tvfas))
+}
+
+console.info('Started')
+console.time('Ended')
+processScript().then(() => console.timeEnd('Ended'))
