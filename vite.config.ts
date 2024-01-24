2c60a4f890b95add8090bff452e11677e454f3d8
Dennis Peters <denjpeters@gmail.com>
2024-01-24 12:02:36 -0500

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,36 +5,12 @@ import dts from 'vite-plugin-dts'
 export default defineConfig({
 	test: {},
 	build: {
-		target: 'ES2020',
+		target: 'es2015',
 		lib: {
 			entry: resolve(__dirname, 'src/main.ts'),
 			name: 'main',
 			fileName: 'main'
-		},
-		// sourcemap: true,
-		// rollupOptions: {
-		// 	external: [
-		// 		// "node:util",
-		// 		// "node:buffer",
-		// 		"node:stream",
-		// 		// "node:net",
-		// 		// "node:url",
-		// 		// "node:fs",
-		// 		// "node:path",
-		// 		// "perf_hooks",
-		// 	],
-		// 	output: {
-		// 		globals: {
-		// 			"node:stream": "stream",
-		// 			// "node:buffer": "buffer",
-		// 			// "node:util": "util",
-		// 			// "node:net": "net",
-		// 			// "node:url": "url",
-		// 			// perf_hooks: "perf_hooks",
-		// 		},
-		// 		inlineDynamicImports: true,
-		// 	},
-		// },
+		}
 	},
 	plugins: [
 		dts()
