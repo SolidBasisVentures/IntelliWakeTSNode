badf0c99087bc46b2fe8374dae8876d9fa6d0e0c
Dennis Peters <denjpeters@gmail.com>
2024-01-24 11:50:06 -0500

    Remove functions and call to stream library

--- vite.config.ts
+++ vite.config.ts
@@ -11,33 +11,32 @@ export default defineConfig({
 			name: 'main',
 			fileName: 'main'
 		},
-		sourcemap: true,
-		rollupOptions: {
-			external: [
-				// "node:util",
-				// "node:buffer",
-				"node:stream",
-				// "node:net",
-				// "node:url",
-				// "node:fs",
-				// "node:path",
-				// "perf_hooks",
-			],
-			output: {
-				globals: {
-					"node:stream": "stream",
-					// "node:buffer": "buffer",
-					// "node:util": "util",
-					// "node:net": "net",
-					// "node:url": "url",
-					// perf_hooks: "perf_hooks",
-				},
-				inlineDynamicImports: true,
-			},
-		},
+		// sourcemap: true,
+		// rollupOptions: {
+		// 	external: [
+		// 		// "node:util",
+		// 		// "node:buffer",
+		// 		"node:stream",
+		// 		// "node:net",
+		// 		// "node:url",
+		// 		// "node:fs",
+		// 		// "node:path",
+		// 		// "perf_hooks",
+		// 	],
+		// 	output: {
+		// 		globals: {
+		// 			"node:stream": "stream",
+		// 			// "node:buffer": "buffer",
+		// 			// "node:util": "util",
+		// 			// "node:net": "net",
+		// 			// "node:url": "url",
+		// 			// perf_hooks: "perf_hooks",
+		// 		},
+		// 		inlineDynamicImports: true,
+		// 	},
+		// },
 	},
 	plugins: [
 		dts()
-
 	]
 })
